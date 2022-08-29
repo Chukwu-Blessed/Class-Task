ef586bf6ef34ea1ac7b00064097c4e1e5f9a8ebb
Chukwu Blessed C <zoddcommunity@gmail.com>
2022-08-29 10:25:31 -0700

    Called the checkFirstAndLastValue function

--- Class/FirstAndLastValue/script.js
+++ Class/FirstAndLastValue/script.js
@@ -2,9 +2,11 @@
 
 const append = document.querySelector( 'div' );
 
-const arr = [2, 3, 7, 4, 2];
+const arr = [2, 3, 7, 4, 2, 3, 2, 9];
 const checkFirstAndLastValue = function ()
 {
-  if ( arr[ 0 ] === arr[ arr.length - 1 ] ) append.innerHTML = 'True';
+  if ( arr[ 0 ] === arr[ arr.length - 1 ] )  append.innerHTML = 'True';
   else append.innerHTML = 'False';
 }
+
+checkFirstAndLastValue();
