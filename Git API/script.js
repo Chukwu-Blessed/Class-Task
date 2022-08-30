606e8b0469dd397028bf36d8620180088bdf09c6
Chukwu Blessed C <zoddcommunity@gmail.com>
2022-08-30 04:55:24 -0700

    Update script.js

--- Git API/script.js
+++ Git API/script.js
@@ -3,7 +3,7 @@
 const searchBar = document.querySelector( '.search_bar' );
 const searchBtn = document.querySelector( '.search_btn' );
 
-const parentEl = document.querySelector('body')
+const parentEl = document.querySelector('.search')
 
 searchBtn.addEventListener( 'click', async function ()
 {
@@ -15,10 +15,16 @@ searchBtn.addEventListener( 'click', async function ()
     // console.log( data );
 
     const markUp = `
-        <h1> ${ data.name } </h1>
-        <img src="${ data.avatar_url }" alt="user_profile_pic">
-        <p> Followers: ${ data.followers } </p>
-        <p> Following: ${ data.following } </p>
+        <div class="body_content">
+          <div class="profile">
+            <h1 class="user_name"> ${ data.name } </h1>
+            <img class="user_profile_pic" src="${ data.avatar_url }" alt="user_profile_pic">
+          </div>
+          <div class="follow">
+            <p class="followers"> Followers: ${ data.followers } </p>
+            <p class="following"> Following: ${ data.following } </p>
+          </div>
+        </div>
       `;
 
     parentEl.insertAdjacentHTML( 'afterend', markUp )
