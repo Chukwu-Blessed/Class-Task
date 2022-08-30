acfc0cf02cdd096c1707c039f76c05f3c740164b
Chukwu Blessed C <zoddcommunity@gmail.com>
2022-08-30 02:29:22 -0700

    Update script.js

--- Git API/script.js
+++ Git API/script.js
@@ -1,28 +1,31 @@
 'use strict'
 
+const searchBar = document.querySelector( '.search_bar' );
+const searchBtn = document.querySelector( '.search_btn' );
+
 const parentEl = document.querySelector('body')
 
-const userName = 'chriscrea8';
-
-const getUser = async function ()
+searchBtn.addEventListener( 'click', async function ()
 {
-  try{
-    const response = await fetch( `https://api.github.com/users/${userName}` );
+  try
+  {
+    const response = await fetch( `https://api.github.com/users/${ searchBar.value }` );
     // console.log( response );
     const data = await response.json();
     // console.log( data );
 
     const markUp = `
-      <h1> ${ data.name } </h1>
-      <img src="${ data.avatar_url }" alt="user_profile_pic">
-      <p> Followers: ${ data.followers } </p>
-      <p> Following: ${ data.following } </p>
-    `;
+        <h1> ${ data.name } </h1>
+        <img src="${ data.avatar_url }" alt="user_profile_pic">
+        <p> Followers: ${ data.followers } </p>
+        <p> Following: ${ data.following } </p>
+      `;
 
-    parentEl.insertAdjacentHTML('beforebegin', markUp)
-  }catch(err) {
-    console.error(err);
+    parentEl.insertAdjacentHTML( 'afterend', markUp )
+
+    searchBar.value = '';
+  } catch ( err )
+  {
+    console.error( err );
   }
-};
-
-getUser();
+} );
